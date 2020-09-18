f31fe91aba1d697ede3ce9d8fce10225551eea8c
XesusGVN <edwardshumkov@gmail.com>
2020-09-18 10:22:05 +0500

    add google speach transcriptior

--- start.js
+++ start.js
@@ -21,7 +21,7 @@ bot.on('voice', async(msg) => {
  const audioID = msg.voice.file_id;
 
   var path = bot.downloadFile(audioID, "./public/audio").then(function (path) {
-    console.log(path);
+    speakMain(path).catch(console.error);
   });
 });
 
@@ -253,6 +253,45 @@ function checkUser(id,type,callback_id, chat_id,msg_id){
   })
 
  
+}
+
+
+
+async function speakMain(filePath) {
+  // Imports the Google Cloud client library
+  const speech = require('@google-cloud/speech');
+  const fs = require('fs');
+
+  // Creates a client
+  const client = new speech.SpeechClient();
+
+  // The name of the audio file to transcribe
+  const fileName = './' + filePath;
+
+  // Reads a local audio file and converts it to base64
+  const file = fs.readFileSync(fileName);
+  const audioBytes = file.toString('base64');
+
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US',
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  // Detects speech in the audio file
+  const [response] = await client.recognize(request);
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+  console.log(`Transcription: ${transcription}`);
 }
 
 
